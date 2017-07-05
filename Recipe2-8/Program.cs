2ca8c6770ab57466e9e55acef36c77f994f86f98
liujiajia <liujiajia@mbinfo.cn>
2017-07-05 17:18:40 +0800

    添加备注

--- Recipe2-8/Program.cs
+++ Recipe2-8/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Recipe2_8
 {
@@ -21,8 +18,8 @@ namespace Recipe2_8
             new Thread(() => Write("Write Thread 1")) { IsBackground = true }.Start();
             new Thread(() => Write("Write Thread 2")) { IsBackground = true }.Start();
 
+            // 30s后主线程结束
             Thread.Sleep(TimeSpan.FromSeconds(30));
-            Console.ReadLine();
         }
 
         static void Read()
@@ -32,15 +29,17 @@ namespace Recipe2_8
             {
                 try
                 {
+                    // 获取读锁（允许多个线程同时获取读锁）
                     _rw.EnterReadLock();
                     foreach (var key in _items)
                     {
-                        Console.WriteLine($"线程 {Thread.CurrentThread.Name} : {key}");
+                        // Console.WriteLine($"线程 {Thread.CurrentThread.Name} : {key}");
                         Thread.Sleep(TimeSpan.FromSeconds(0.1));
                     }
                 }
                 finally
                 {
+                    // 在finally中释放锁，确保锁最终会被释放
                     _rw.ExitReadLock();
                 }
             }
@@ -53,17 +52,20 @@ namespace Recipe2_8
                 try
                 {
                     int newKey = new Random().Next(250);
+                    // 获取可升级读锁
                     _rw.EnterUpgradeableReadLock();
                     if (!_items.ContainsKey(newKey))
                     {
                         try
                         {
+                            // 等待所有的读锁释放后获取写锁，此时所有的获取读锁操作会被阻塞
                             _rw.EnterWriteLock();
                             _items[newKey] = 1;
                             Console.WriteLine($"新Key {newKey} 被 {threadName} 加入字典");
                         }
                         finally
                         {
+                            // 在finally中释放锁，确保锁最终会被释放
                             _rw.ExitWriteLock();
                         }
                     }
@@ -71,6 +73,7 @@ namespace Recipe2_8
                 }
                 finally
                 {
+                    // 在finally中释放锁，确保锁最终会被释放
                     _rw.ExitUpgradeableReadLock();
                 }
             }
