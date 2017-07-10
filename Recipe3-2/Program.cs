4cb79dceff7dc241660c131b155b8dcfb7ee2f9e
liujiajia <liujiajia@mbinfo.cn>
2017-07-10 16:18:42 +0800

    add 3-2 3-3

---
+++ Recipe3-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Recipe3_2
+{
+    class Program
+    {
+        /// <summary>
+        /// 向线程池中放入异步操作
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            const int x = 1;
+            const int y = 2;
+            const string lambdaState = "lambda state 2";
+
+            // 使用QueueUserWorkItem方法将AsyncOperation方法放到线程池中
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            // 带参数的调用（参数值传递到AsyncOperation方法的参数object state）
+            ThreadPool.QueueUserWorkItem(AsyncOperation, "async state");
+            // 使线程睡眠，从而让线程池拥有为新操作重用线程的可能性
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+
+            // 使用lambda表达式放置到线程池
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine($"操作状态：{state}");
+                Console.WriteLine($"当前工作线程Id：{Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+
+            // 这个lambda表达式中使用了闭包机制，从而无须传递lambda表达式的状态
+            // 闭包更灵活，允许我们向异步方法传递一个以上的对象，而且这些对象具有静态类型
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Console.WriteLine($"操作状态：{x + y}, {lambdaState}");
+                Console.WriteLine($"当前工作线程Id：{Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            });
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
+        private static void AsyncOperation(object state)
+        {
+            Console.WriteLine($"操作状态：{state ?? "(null)"}");
+            Console.WriteLine($"当前工作线程Id：{Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+    }
+}
---
+++ Recipe3-3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Recipe3_3
+{
+    class Program
+    {
+        /// <summary>
+        /// 线程池和并行度
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            const int numberOfOpeartions = 500;
+            var sw = new Stopwatch();
+            sw.Start();
+            UseThreads(numberOfOpeartions);
+            sw.Stop();
+            Console.WriteLine($"使用线程的执行时间：{sw.ElapsedMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            UseThreadPool(numberOfOpeartions);
+            sw.Stop();
+            Console.WriteLine($"使用线程池的执行时间：{sw.ElapsedMilliseconds}ms");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 使用线程
+        /// </summary>
+        /// <param name="numberOfOperations"></param>
+        static void UseThreads(int numberOfOperations)
+        {
+            using (var countdown = new CountdownEvent(numberOfOperations))
+            {
+                Console.WriteLine("通过创建线程执行处理");
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    var thread = new Thread(() =>
+                    {
+                        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                    thread.Start();
+                }
+                countdown.Wait();
+
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 使用线程池
+        /// </summary>
+        /// <param name="numberOfOperations"></param>
+        static void UseThreadPool(int numberOfOperations)
+        {
+            using (var countdown = new CountdownEvent(numberOfOperations))
+            {
+                Console.WriteLine("在一个线程池中执行处理");
+                for (int i = 0; i < numberOfOperations; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(_ => {
+                        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
+                        countdown.Signal();
+                    });
+                }
+                countdown.Wait();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
