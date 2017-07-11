e3150bae5e0cfe703a0d19325611860298083352
liujiajia <liujiajia@mbinfo.cn>
2017-07-11 09:26:15 +0800

    add 3-4 3-5

---
+++ Recipe3-4/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Recipe3_4
+{
+    class Program
+    {
+        /// <summary>
+        /// 实现一个取消选项
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation1(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            using (var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation2(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            using (var cts = new CancellationTokenSource())
+            {
+                CancellationToken token = cts.Token;
+                ThreadPool.QueueUserWorkItem(_ => AsyncOperation3(token));
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                cts.Cancel();
+            }
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.ReadLine();
+        }
+
+        static void AsyncOperation1(CancellationToken token)
+        {
+            Console.WriteLine("开始第一个任务");
+            for (int i = 0; i < 5; i++)
+            {
+                // 轮询检查token.IsCancellationRequested，如果为true，说明操作需要被取消
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("第一个任务已经被取消");
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            Console.WriteLine("第一个任务已经成功完成");
+        }
+
+        static void AsyncOperation2(CancellationToken token)
+        {
+            try
+            {
+                Console.WriteLine("开始第二个任务");
+                for (int i = 0; i < 5; i++)
+                {
+                    // 如果已取消，则抛出异常
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+                Console.WriteLine("第二个任务已经成功完成");
+            }
+            catch (OperationCanceledException)
+            {
+                // 在主处理意外处理取消过程
+                Console.WriteLine("第二个任务已经被取消");
+            }
+        }
+
+        static void AsyncOperation3(CancellationToken token)
+        {
+            bool cancelltionFlag = false;
+            // 注册一个回调函数
+            // 当操作被取消时，线程池将调用该回调函数
+            token.Register(() => cancelltionFlag = true);
+            Console.WriteLine("开始第三个任务");
+            for (int i = 0; i < 5; i++)
+            {
+                if (cancelltionFlag)
+                {
+                    Console.WriteLine("第三个任务已经被取消");
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            Console.WriteLine("第三个任务已经成功完成");
+        }
+    }
+}
---
+++ Recipe3-5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+namespace Recipe3_5
+{
+    class Program
+    {
+        /// <summary>
+        /// 在线程中使用等待事件处理器及超时
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            RunOperations(TimeSpan.FromSeconds(5));
+            RunOperations(TimeSpan.FromSeconds(7));
+        }
+
+        static void RunOperations(TimeSpan workerOperationTimeout)
+        {
+            using (var evt = new ManualResetEvent(false))
+            using (var cts = new CancellationTokenSource())
+            {
+                Console.WriteLine($"注册一个超时操作...");
+                // 该方法允许我们将回调函数放入线程池中的队列中。
+                // 当提供的等待时间处理器收到信号或发生超时时，该回调函数将被调用。
+                // 第一个参数是等待对象（System.Threading.WaitHandle）
+                // 第二个参数是回调函数
+                // 第四个参数是超时事件
+                // 第五个参数为true，表示仅执行一次
+                var worker = ThreadPool.RegisterWaitForSingleObject(evt,
+                    (state, isTimeOut) => WorkerOperationWait(cts, isTimeOut),
+                    null,
+                    workerOperationTimeout,
+                    true);
+
+                Console.WriteLine("开始执行一个长操作");
+                ThreadPool.QueueUserWorkItem(_ => WorkerOperation(cts.Token, evt));
+
+                Thread.Sleep(workerOperationTimeout.Add(TimeSpan.FromSeconds(2)));
+                worker.Unregister(evt);
+            }
+        }
+
+        static void WorkerOperation(CancellationToken token, ManualResetEvent evt)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                // 判断token是否已取消
+                if (token.IsCancellationRequested)
+                {
+                    return;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            // 6秒后发送事件结束信号
+            evt.Set();
+        }
+
+        static void WorkerOperationWait(CancellationTokenSource cts, bool isTimeOut)
+        {
+            if (isTimeOut)
+            {
+                // 如果操作超时，则发送取消信号
+                cts.Cancel();
+                Console.WriteLine("操作超时且被取消");
+            } else
+            {
+                Console.WriteLine("操作执行成功");
+            }
+        }
+    }
+}
