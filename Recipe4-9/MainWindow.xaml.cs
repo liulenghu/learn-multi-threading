87632f1bd3ee771ac9feab3e45984fd843090990
liujiajia <liujiajia@mbinfo.cn>
2017-07-28 13:48:12 +0800

    add 4-9 5-1

--- Recipe4-9/MainWindow.xaml.cs
+++ Recipe4-9/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Recipe4_9
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// 使用 TaskScheduler 配置任务的执行
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -32,11 +22,15 @@ namespace Recipe4_9
             try
             {
                 // string result = TaskMethod(TaskScheduler.FromCurrentSynchronizationContext()).Result;
+                // 同步调用，当前用户界面会被冻结，在任务执行完毕前无法响应任何操作
+                // TaskMethod中的Task线程无法UI线程中的控件ContentTextBlock，导致发生异常
                 string result = TaskMethod().Result;
+                // 该赋值不会被执行到
                 ContentTextBlock.Text = result;
             }
             catch (Exception ex)
             {
+                // 出异常时显示异常消息
                 ContentTextBlock.Text = ex.InnerException.Message;
             }
         }
@@ -45,6 +39,8 @@ namespace Recipe4_9
         {
             ContentTextBlock.Text = string.Empty;
             Mouse.OverrideCursor = Cursors.Wait;
+            // 异步执行Task，用户界面不会被冻结，任务执行期间界面仍然可以响应用户操作
+            // 但是仍然会发生异常
             Task<string> task = TaskMethod();
             task.ContinueWith(
                 t =>
@@ -61,6 +57,8 @@ namespace Recipe4_9
         {
             ContentTextBlock.Text = string.Empty;
             Mouse.OverrideCursor = Cursors.Wait;
+
+            // 将UI线程任务调度程序(TaskScheduler.FromCurrentSynchronizationContext())提供给了任务
             Task<string> task = TaskMethod(TaskScheduler.FromCurrentSynchronizationContext());
             task.ContinueWith(
                 t => Mouse.OverrideCursor = null,
---
+++ Recipe5-1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Recipe5_1
+{
+    class Program
+    {
+        /// <summary>
+        /// 使用await操作符获取异步任务结果
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Task t = AsynchronyWithTPL();
+            t.Wait();
+
+            t = AsynchronyWithAwait();
+            t.Wait();
+
+            Console.ReadLine();
+        }
+
+        // 标准TPL的写法
+        static Task AsynchronyWithTPL()
+        {
+            Task<string> t = GetInfoAsync("Task 1");
+            Task t2 = t.ContinueWith(task => Console.WriteLine(t.Result),
+                TaskContinuationOptions.NotOnFaulted);
+            Task t3 = t.ContinueWith(task => Console.WriteLine(t.Exception.InnerException),
+                TaskContinuationOptions.OnlyOnFaulted);
+            return Task.WhenAny(t2, t3);
+        }
+
+        // 使用 async await 关键字的写法
+        static async Task AsynchronyWithAwait()
+        {
+            try
+            {
+                string result = await GetInfoAsync("Task 2");
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static async Task<string> GetInfoAsync(string name)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}. " +
+                $"Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}";
+        }
+    }
+}
