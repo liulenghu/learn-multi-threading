6669565aa108d3896a5f5a0c86e126243dbafb13
liujiajia <liujiajia@mbinfo.cn>
2017-08-24 13:48:40 +0800

    add 7-2 7-3

---
+++ Recipe7-2/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace Recipe7_2
+{
+    class Program
+    {
+        /// <summary>
+        /// PLINQ查询
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            // 正常的顺序LINQ查询
+            // 所有操作都运行在当前线程
+            var query = from t in GetTypes()
+                        select EmulateProcessing(t);
+
+            foreach (string typeName in query)
+            {
+                PrintInfo(typeName);
+            }
+
+            sw.Stop();
+            Console.WriteLine("---");
+            Console.WriteLine("Sequential LINQ query.");
+            Console.WriteLine("正常的顺序LINQ查询");
+            Console.WriteLine("所有操作都运行在当前线程");
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            Console.WriteLine("Press Enter to continue....");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+
+            sw.Start();
+            // 使用AsParallel方法将查询并行化
+            // 默认情况下结果会被合并到单个线程中
+            var paralleQuery = from t in GetTypes().AsParallel()
+                               select EmulateProcessing(t);
+            foreach (var typeName in paralleQuery)
+            {
+                PrintInfo(typeName);
+            }
+            sw.Stop();
+            Console.WriteLine("---");
+            Console.WriteLine("Parallel LINQ query. The results are being merged on a single thrad");
+            Console.WriteLine("使用AsParallel方法将查询并行化");
+            Console.WriteLine("默认情况下结果会被合并到单个线程中");
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            Console.WriteLine("Press Enter to continue....");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+
+
+            sw.Start();
+            // 使用AsParallel方法将查询并行化
+            paralleQuery = from t in GetTypes().AsParallel()
+                           select EmulateProcessing(t);
+            // 使用ForAll方法将打印操作和查询操作放到了同一个线程，跳过了结果合并的步骤
+            paralleQuery.ForAll(PrintInfo);
+
+            sw.Stop();
+            Console.WriteLine("---");
+            Console.WriteLine("Parallel LINQ query. The results are being processed in parallel");
+            Console.WriteLine("使用ForAll方法将打印操作和查询操作放到了同一个线程，跳过了结果合并的步骤");
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            Console.WriteLine("Press Enter to continue....");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+
+
+            sw.Start();
+            // 使用AsSequential方法将PLINQ查询已顺序方式执行
+            query = from t in GetTypes().AsParallel().AsSequential()
+                    select EmulateProcessing(t);
+            foreach (string typeName in query)
+            {
+                PrintInfo(typeName);
+            }
+
+            sw.Stop();
+            Console.WriteLine("---");
+            Console.WriteLine("Parallel LINQ query, transformed into sequential.");
+            Console.WriteLine("使用AsSequential方法将PLINQ查询以顺序方式执行");
+            Console.WriteLine("运行结果同第一个示例完全一样");
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            Console.WriteLine("Press Enter to continue....");
+            Console.ReadLine();
+            Console.Clear();
+            sw.Reset();
+        }
+
+        static void PrintInfo(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(15));
+            Console.WriteLine($"{typeName} type was printed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static string EmulateProcessing(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            Console.WriteLine($"{typeName} type was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+            return typeName;
+        }
+
+        /// <summary>
+        /// 使用反射API查询加载到当前应用程序域中的所有组件中名称以“Web”开头的类型
+        /// </summary>
+        /// <returns></returns>
+        static IEnumerable<string> GetTypes()
+        {
+            return from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                   from type in assembly.GetExportedTypes()
+                   where type.Name.StartsWith("Web")
+                   select type.Name;
+        }
+    }
+}
---
+++ Recipe7-3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Recipe7_3
+{
+    class Program
+    {
+        /// <summary>
+        /// 调整PLINQ查询的参数
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var parallelQuery = from t in GetTypes().AsParallel()
+                                select EmulateProcessing(t);
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromSeconds(3));
+
+            try
+            {
+                parallelQuery
+                    // 设置要在查询中使用的并行度。并行度是将用于处理查询的同时执行的任务的最大数目。
+                    .WithDegreeOfParallelism(Environment.ProcessorCount)
+                    // 设置查询的执行模式
+                    // （ForceParallelism：并行化整个查询，即使要使用系统开销大的算法。）
+                    .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                    // 设置此查询的合并选项，它指定查询对输出进行缓冲处理的方式。
+                    // (Default:使用默认合并类型，即 AutoBuffered。)
+                    // (AutoBuffered:利用系统选定大小的输出缓冲区进行合并。在向查询使用者提供结果之前，会先将结果累计到输出缓冲区中。)
+                    .WithMergeOptions(ParallelMergeOptions.Default)
+                    // 设置要与查询关联的 System.Threading.CancellationToken。
+                    .WithCancellation(cts.Token)
+                    .ForAll(Console.WriteLine);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("---");
+                Console.WriteLine("操作已被取消");
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine("执行未排序的PLINQ查询");
+
+            var unorderedQuery = from i in ParallelEnumerable.Range(1, 30)
+                                 select i;
+
+            foreach (var i in unorderedQuery)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine("执行已排序的PLINQ查询");
+            var orderedQuery = from i in ParallelEnumerable.Range(1, 30).AsOrdered()
+                               select i;
+
+            foreach (var i in orderedQuery)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+
+        static string EmulateProcessing(string typeName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(250,350)));
+            Console.WriteLine($"{typeName} type was processed on a thread id {Thread.CurrentThread.ManagedThreadId}");
+            return typeName;
+        }
+
+        /// <summary>
+        /// 使用反射API查询加载到当前应用程序域中的所有组件中名称以“Web”开头的类型
+        /// </summary>
+        /// <returns></returns>
+        static IEnumerable<string> GetTypes()
+        {
+            return from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                   from type in assembly.GetExportedTypes()
+                   where type.Name.StartsWith("Web")
+                   orderby type.Name.Length
+                   select type.Name;
+        }
+    }
+}
